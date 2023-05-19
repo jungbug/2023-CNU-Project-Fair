74f7ec4c3d06105acc654324cc7d94d8564adb30
JUNG SUNG YOON <xbox1111@naver.com>
2023-05-20 01:52:37 +0900

    add: img

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const imgs = document.querySelectorAll('img');
+
+for (let i = 1; i < imgs.length; i++) {
+  imgs[i].addEventListener('click', function () {
+    if (imgs[i].classList.contains('active-img')) {
+      imgs[i].classList.remove('active-img');
+    }
+    else {
+      imgs[i].classList.add('active-img');
+    }
+  })
+};
