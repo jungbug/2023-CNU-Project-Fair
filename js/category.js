6d7fbe29ec124e7a7f85ef69317d76615e1fa79b
Kosw <swko4512@gmail.com>
2023-05-21 03:03:09 +0900

    여러가지

--- js/category.js
+++ js/category.js
@@ -1,5 +1,16 @@
+const imgs = document.querySelectorAll('img');
 const previousButton = document.getElementById('previous-button');
 const nextButton = document.getElementById('next-button');
+for (let i = 1; i < imgs.length; i++) {
+  imgs[i].addEventListener('click', function () {
+    if (imgs[i].classList.contains('active-img')) {
+      imgs[i].classList.remove('active-img');
+    }
+    else {
+      imgs[i].classList.add('active-img');
+    }
+  })
+};
 previousButton.addEventListener('click', () => {
 window.location.href = 'index.html';
 });
